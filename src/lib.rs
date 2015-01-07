4e3bc5e4d1efc5937616fdc48ed42534b2a300aa
Nathan Sizemore <nathanrsizemore@gmail.com>
2015-01-07 14:30:12 -0500

    Rust nightly changes for trait bounds Sized? T -> T: ?Sized

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //#![warn(missing_doc)]
 #![forbid(non_camel_case_types)]
-#![feature(macro_rules, slicing_syntax, associated_types)]
+#![feature(slicing_syntax)]
 
 //! This crate currently provides almost XML 1.0/1.1-compliant pull parser.
 
