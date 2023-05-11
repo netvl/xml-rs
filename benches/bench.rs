813e9edfd745d4a4f8a62105c87c823ece26d59d
Kornel <kornel@geekhood.net>
2023-05-11 13:53:35 +0100

    Bench writing

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,15 +2,30 @@
 
 extern crate test;
 use test::Bencher;
-use xml::EventReader;
+use xml::{EventReader, EventWriter};
 
 #[bench]
-fn bla(bencher: &mut Bencher) {
+fn read(bencher: &mut Bencher) {
     let xml = std::fs::read("tests/documents/sample_1.xml").unwrap();
     bencher.iter(move || {
         let parser = EventReader::new(xml.as_slice());
         for e in parser {
             e.unwrap();
         }
+    });
+}
+
+#[bench]
+fn write(bencher: &mut Bencher) {
+    let xml = std::fs::read("tests/documents/sample_1.xml").unwrap();
+    let events: Vec<_> = EventReader::new(xml.as_slice()).into_iter().map(|e| e.unwrap()).collect();
+    let events: Vec<_> = events.iter().filter_map(|e| e.as_writer_event()).collect();
+
+    bencher.iter(move || {
+        let mut serializer = EventWriter::new(Vec::new());
+        for e in &events {
+            serializer.write((*e).clone()).unwrap();
+        }
+        serializer.into_inner()
     });
 }
